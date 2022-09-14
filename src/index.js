6f05efdbe4456af525ef838229e9529986c4b9e1
mjallo <90021166+mjallo@users.noreply.github.com>
2022-09-14 23:58:13 +0200

    comments

--- src/index.js
+++ src/index.js
@@ -3,7 +3,3 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-//CHALLENGE: Make the code in App.jsx work.
-//The final app should have a single contact
-//with fName, lName and email.
